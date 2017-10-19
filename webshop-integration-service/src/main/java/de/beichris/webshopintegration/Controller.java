b5d980382cc8d60b1551a417a6b141db603838f8
bierbierbier <nadinegrabmair@yahoo.de>
2017-10-19 22:19:19 +0200

    Starter Project

---
+++ webshop-integration-service/src/main/java/de/beichris/webshopintegration/Controller.java
@@ -0,0 +1,13 @@
+package de.beichris.webshopintegration;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
